0bb8ef0a6b6cbdc275988fde2510a38fa127053c
Matt Neznanski <mneznanski@eomediagroup.com>
2016-04-07 16:37:35 -0700

    Remove json package

--- fetcher.py
+++ fetcher.py
@@ -2,8 +2,6 @@ from urllib2 import urlopen
 import requests
 from xml.etree import ElementTree
 
-# from json import load 
-
 url = 'http://tidesandcurrents.noaa.gov/api/datagetter?' 
 begin_date = 'begin_date=20151101'
 end_date = 'end_date=20151130'
