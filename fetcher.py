23c2f74e9be4db77e0aa98af4beee6a31167c282
Matt Neznanski <mneznanski@eomediagroup.com>
2016-04-12 13:55:40 -0700

    add timeframe function to format date range

--- fetcher.py
+++ fetcher.py
@@ -1,13 +1,25 @@
 import requests
 from xml.etree import ElementTree as ET
+import datetime
+import calendar
 
-url = 'http://tidesandcurrents.noaa.gov/api/datagetter?' 
-begin_date = 'begin_date=20151101'
-end_date = 'end_date=20151130'
+def timeframe():
+    year = datetime.date.today().year
+    userin = int(raw_input("Enter a month as a number: "))
+    monthdays = format(calendar.monthrange(year, userin)[1])
+    if len(str(userin)) == 1:
+        twodigmonth = '0' + str(userin)
+    else:
+        twodigmonth = str(userin)
+    
+    return "begin_date=" + str(year) + twodigmonth + "01&end_date=" + str(year) + twodigmonth + monthdays 
+
 station = '&station=9437540'
 product = '&product=predictions'
 datum = '&datum=mllw'
-url = url + begin_date + '&' + end_date
+
+url = 'http://tidesandcurrents.noaa.gov/api/datagetter?' 
+url = url + timeframe()
 url += station + product + datum
 url += '&interval=h&units=english&time_zone=lst_ldt&application=EOMG&format=xml'
 
